f047e6952c10761c70a591a88d182be021102bff
MisteryDevs <officialbotdev@gmail.com>
2025-04-03 14:02:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", 29893020))
 API_HASH = getenv("API_HASH", "28e79037f0b334ef0503466c53f08af5")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", 7006524418))
+OWNER_ID = int(getenv("OWNER_ID", "5738579437"))
 SUPPORT_GRP = "ur_support07"
 UPDATE_CHNL = "ur_rishu_143"
 OWNER_USERNAME = "rishu1286"

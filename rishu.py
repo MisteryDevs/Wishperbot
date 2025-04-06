1cd0ac8ce588bbb5180a4cde7c29420912b03008
MisteryDevs <officialbotdev@gmail.com>
2025-04-06 06:22:50 +0530

    Update rishu.py

--- rishu.py
+++ rishu.py
@@ -14,7 +14,7 @@ from config import API_ID, API_HASH, BOT_TOKEN
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority")
 
-client["WhisperBot"]
+
 users_col = db["users"]
 
 OWNER_ID = 5738579437
